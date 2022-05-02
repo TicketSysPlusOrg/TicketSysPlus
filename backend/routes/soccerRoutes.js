22e95e186d9256c31e979415d60e7ca8bd11b428
Conor O'Brien <91850829+conorep@users.noreply.github.com>
2022-05-02 16:37:25 -0700

    server files

---
+++ backend/routes/soccerRoutes.js
@@ -0,0 +1,27 @@
+import {
+  addNewPlayer,
+  deletePlayer,
+  getPlayers,
+  getPlayerWithID,
+  updatePlayer,
+} from '../controllers/playerControllers';
+
+const routes = (app) => {
+  app
+    .route('/players')
+    //get endpoint
+    .get(getPlayers)
+    //this is the POST endpoint
+    .post(addNewPlayer);
+
+  app
+    .route('/players/:PlayerId')
+    //get specific player
+    .get(getPlayerWithID)
+    //update specific player
+    .put(updatePlayer)
+    //delete specific player
+    .delete(deletePlayer);
+};
+
+export default routes;
