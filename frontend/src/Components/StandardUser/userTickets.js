16ccd2996591489737e262d56d5ee5d49c234ed2
conor <Obrien.Conor@student.greenriver.edu>
2022-05-10 12:10:07 -0700

    can now display all cards on standarduser screen. need to style everything and add the ability to block or cancel tickets.

--- frontend/src/Components/StandardUser/userTickets.js
+++ frontend/src/Components/StandardUser/userTickets.js
@@ -1,5 +1,43 @@
 //list of all of the current standard user's tickets
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import {Card, Button, Col} from 'react-bootstrap';
 
-function TSPlist(props) {
+
+function TSPlist() {
+    const [ticketArray, setTickets] = useState([]);
+
+    axios
+        .get('http://localhost:4001/tix')
+        .then((res) => {
+            setTickets(res.data);
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+
+    return (
+        <>
+            <h4>Tickets</h4>
+
+            {ticketArray.map((thisTicket) => (
+                <Col sm={3} >
+                    <Card className="my-3 mx-4" key={thisTicket._id}>
+                        <Card.Body>
+                            <Card.Title>{thisTicket.title}</Card.Title>
+                            <Card.Text>
+                                {thisTicket.description}
+                            </Card.Text>
+                            <Button variant="primary">Block Ticket</Button>
+                            <Button variant="secondary">Cancel Ticket</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+            ))}
+        </>
+        )
+
 }
+
+export default TSPlist;
