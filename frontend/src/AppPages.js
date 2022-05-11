632c5240f61ce4aec37dd27ae67a60f66f213d55
granny <granny@pl3x.net>
2022-05-10 22:52:30 -0700

    fix linter errors

--- frontend/src/AppPages.js
+++ frontend/src/AppPages.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Routes, Route } from 'react-router';
+import React from "react";
+import { Routes, Route } from "react-router";
 import { MsalProvider } from "@azure/msal-react";
 import StandardUser from "./Components/TicketSysPlusPages/StandardUser";
 import Admin from "./Components/TicketSysPlusPages/Admin";
 import Error from "./Components/TicketSysPlusPages/ErrorPage";
 import TSPAppFetched from "./Components/TicketSysPlusPages/TSPAppFetched";
+import PropTypes from "prop-types";
+
 
 function MainApp({ pca }) {
-        return (
-            <MsalProvider instance={pca}>
-                    <Routes>
-                        <Route path="/" element={ <StandardUser />} exact/>
-                        <Route path="/admin" element={ <Admin />}/>
-                        <Route path="/test" element={<TSPAppFetched />} />
-                        <Route path="*" element={ <Error />}/>
-                    </Routes>
-            </MsalProvider>
-        );
+    return (
+        <MsalProvider instance={pca}>
+            <Routes>
+                <Route path="/" element={ <StandardUser />} exact/>
+                <Route path="/admin" element={ <Admin />}/>
+                <Route path="/test" element={<TSPAppFetched />} />
+                <Route path="*" element={ <Error />}/>
+            </Routes>
+        </MsalProvider>
+    );
 }
+
+MainApp.propTypes = {
+    pca: PropTypes.object
+};
 
 export default MainApp;
