dcc00e0a03ea4fc07b2934a21095a939c8e38725
DarkSafeguard <imhank@me.com>
2022-05-15 16:25:11 -0700

    Created seperate AdminJsonDisplay.js component to display json data, save, import, export buttons. Modified Admin.js to include component

--- frontend/src/Components/AdminUser/AdminJsonDisplay.js
+++ frontend/src/Components/AdminUser/AdminJsonDisplay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import fetchData from "../APIActions/FetchData";
 import "../TicketSysPlusPages/TSPApp.css";
 
@@ -7,13 +7,25 @@ const fetchDataPromise = fetchData("ticketInfo");
 function AdminJson() {
 
     const dataDetails = fetchDataPromise.read();
+    const json = JSON.stringify(dataDetails, null, 3);
+    const [change, setChange] = useState(true);
+
+    function verify() {
+        const jsonText = document.getElementById("jsonText").value;
+        if (jsonText !== json){
+            setChange(false);
+        }
+        else{
+            setChange(true);
+        }
+    }
 
     return (
         <>
             <div className="row align-items-center justify-content-center mt-5">
                 <div className="col-7 d-flex mb-1">
                     {/*TODO: Change buttons to desirable color scheme*/}
-                    <button className="btn btn-danger mx-3" id="savebtn" type="button">Save</button>
+                    <button className="btn btn-danger mx-3" id="savebtn" type="button" disabled={change}>Save</button>
                     <button className="btn btn-danger mx-3" id="importbtn" type="button">Import</button>
                     <button className="btn btn-danger mx-3" id="exportbtn" type="button">Export</button>
                 </div>
@@ -21,8 +33,8 @@ function AdminJson() {
 
                 <div className="col-7 text-center border border-3 border-info p-3 bg-white shadow-lg">
                     {/*TODO: Make height responsive to text area content (scrollHeight). also set this JSONstringify to defaultvalue or value of textarea*/}
-                    <textarea style={{height: "400px"}} name="ticketData" className="text-wrap text-break w-100">
-                        {JSON.stringify(dataDetails, null, 3)}
+                    <textarea onChange={() => verify()} style={{height: "400px"}} name="ticketData" id={"jsonText"} className="text-wrap text-break w-100">
+                        {json}
                     </textarea>
                 </div>
             </div>
