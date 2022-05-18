4345166b675a9b7446da12f26a4a47f69a275a05
granny <granny@pl3x.net>
2022-05-17 17:53:16 -0700

    fix some linter errors

--- frontend/src/Components/TicketSysPlusPages/ErrorComponent.js
+++ frontend/src/Components/TicketSysPlusPages/ErrorComponent.js
@@ -1,3 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
 export const ErrorComponent = ({error}) => {
     return <p>An Error Occurred: {error.errorCode}</p>;
+};
+
+ErrorComponent.propTypes = {
+    error: PropTypes.object
 };
