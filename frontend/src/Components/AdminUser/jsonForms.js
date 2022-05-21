128af3fc90b4b952342ba4a5ea7e0a0f041e52fa
DarkSafeguard <imhank@me.com>
2022-05-20 21:52:55 -0700

    On json submission, old db json is deleted and replaced

--- frontend/src/Components/AdminUser/jsonForms.js
+++ frontend/src/Components/AdminUser/jsonForms.js
@@ -15,6 +15,15 @@ function JsonForm({jsonModal}) {
         const jsonTitle = inputTitle.current.value;
         const jsonBody = inputBody.current.value;
 
+        axios.delete("http://localhost:4001/jsons")
+            .then((res) => {
+                console.log(res);
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
         axios.post("http://localhost:4001/jsons", {
             title: jsonTitle,
             body: jsonBody
