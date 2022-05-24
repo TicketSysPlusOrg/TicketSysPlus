bea8daa3def8d5d194a7542654e8726bc68fda0e
DarkSafeguard <imhank@me.com>
2022-05-24 13:19:39 -0700

    old Json data can be loaded from database to textarea to be saved.

--- frontend/src/Components/AdminUser/jsonForms.js
+++ frontend/src/Components/AdminUser/jsonForms.js
@@ -3,16 +3,14 @@ import React, { createRef, useState } from "react";
 import axios from "axios";
 import { Button, Form } from "react-bootstrap";
 
-function JsonForm({ jsonModal }) {
+function JsonForm({jsonModal}) {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
 
-    let inputTitle = createRef();
     let inputBody = createRef();
 
     function submitJson(SubmitEvent) {
 
-        const jsonTitle = inputTitle.current.value;
         const jsonBody = inputBody.current.value;
 
         axios.delete("https://backend.granny.dev/jsons")
@@ -39,18 +37,10 @@ function JsonForm({ jsonModal }) {
     return (
         <>
             <div className="row">
-                <div className="col ">
+                <div className="col">
 
                     {/*TODO: validation  for all fields*/}
                     <Form className="col s12" onSubmit={submitJson}>
-                        <div className="row mb-2">
-                            <Form.Group className="col s6">
-                                <Form.Label htmlFor="jsonTitle">JSON Title</Form.Label>
-                                <Form.Control type="text" placeholder="Enter title" ref={inputTitle} />
-                                <Form.Text id="jsonTitle" name="jsonTitle" />
-                            </Form.Group>
-                        </div>
-
                         {/*Will output JSON file here*/}
                         <div className="row mb-2">
                             <Form.Group className="col s6">
