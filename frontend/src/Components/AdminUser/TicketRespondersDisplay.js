363c45504a643f997a798c5c384e294fb82188ce
DarkSafeguard <imhank@me.com>
2022-06-01 13:56:04 -0700

    removed import of TSP app.css stylesheet as it no longer exists to fix display error

--- frontend/src/Components/AdminUser/TicketRespondersDisplay.js
+++ frontend/src/Components/AdminUser/TicketRespondersDisplay.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import fetchData from "../APIActions/FetchData";
-import "../TicketSysPlusPages/TSPApp.css";
 import Responders from "./Responders";
 
 function TicketResponders(props) {
