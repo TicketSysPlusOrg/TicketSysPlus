0d142724db5fed2a189cdb7891cfd922ad14452f
granny <granny@pl3x.net>
2022-06-01 17:13:01 -0700

    fix whitespace and indentation

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ mongoose.connect("mongodb://127.0.0.1:27017", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("Successfully connected to MongoDB Database"))
-.catch(error => console.log("Could not connect to MongoDB Database"));
+    .catch(error => console.log("Could not connect to MongoDB Database"));
 
 //bodyparser setup
 app.use(bodyparser.urlencoded({ extended: true }));
