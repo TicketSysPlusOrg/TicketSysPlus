02fa2154002be7cc7990c830c4b5cbbdd11de2f0
conor <Obrien.Conor@student.greenriver.edu>
2022-06-03 20:46:38 -0700

    added block ticket ability to work items, legitimately rerenders ticket upon state change. added hoverover styles and title attributes to anchors to help user understand.

--- frontend/src/Components/StandardUser/userTickets.js
+++ frontend/src/Components/StandardUser/userTickets.js
@@ -7,6 +7,7 @@ import SingleTicket from "./singleTicket";
 import { AiFillEye } from "react-icons/ai";
 import { FaPencilAlt } from "react-icons/fa";
 import { TiArrowForwardOutline } from "react-icons/ti";
+import { FiAlertOctagon } from "react-icons/fi";
 import TicketForm from "./ticketForms";
 
 function UserTickets(props) {
@@ -29,6 +30,7 @@ function UserTickets(props) {
     const [devOpsTix, setDevOpsTix] = useState(null);
     const [noTickets, setNoTickets] = useState(null);
     const [activeProj, setActiveProj] = useState(null);
+    const [activePrjID, setActivePrjId] = useState(null);
 
     useEffect(() => {
         run();
@@ -40,6 +42,7 @@ function UserTickets(props) {
         const getProj = await azureConnection.getProject(props.projects[0]);
         console.log(getProj);
         setActiveProj(getProj.name);
+        setActivePrjId(getProj.id);
 
         const allWorkItems = await azureConnection.getPrjWorkItems(getProj.name, getProj.defaultTeam.id);
 
@@ -64,6 +67,20 @@ function UserTickets(props) {
     const [renderEdit, setRenderEdit] = useState(null);
     const [allTicketInfo, setAllTicketInfo] = useState(null);
 
+    /*set this ticket's state to blocked*/
+    async function blockTicket(itemID) {
+        const blockTicket = {"System.State": "Blocked"};
+        const updateTicket = azureConnection.updateWorkItem(activePrjID, itemID, {"fields": blockTicket});
+    }
+
+    /*trigger tickets rerender on state change (i.e. changing to blocked*/
+    const [blockStateChange, setBlockStateChange] = useState(null);
+
+    /*run render again*/
+    useEffect(() => {
+        run();
+    }, [blockStateChange !== null]);
+
     return (
         <>
             <h4 className={"mt-4"}>Displaying Tickets for: {activeProj}</h4>
@@ -76,10 +93,10 @@ function UserTickets(props) {
                         <Col xs={12} key={index}>
                             {/*TODO: double check that areapath will always be filled*/}
                             <div className={"projectSelect " }>
-                                <Container className={"my-1 py-1 px-0 row bg-white"} >
+                                <Container className={"my-1 py-1 px-0 row hoverOver"} >
                                     <Col xs={1} className={"align-self-center"}>
                                         <div>
-                                            <a onClick={() => {setRenderEdit(false); showTicketModal(devTix.fields["System.AreaPath"] + "|" + devTix.id);}} className={"eyeSee"}><AiFillEye size={"2rem"} /></a>
+                                            <a title={"Inspect Ticket"} onClick={() => {setRenderEdit(false); showTicketModal(devTix.fields["System.AreaPath"] + "|" + devTix.id);}} className={"eyeSee"}><AiFillEye size={"2rem"} /></a>
                                         </div>
                                     </Col>
                                     <Col xs={3}>
@@ -87,12 +104,12 @@ function UserTickets(props) {
                                             <strong >{devTix.fields["System.Title"]}</strong>
                                         </div>
                                     </Col>
-                                    <Col xs={7}>
+                                    <Col xs={6}>
                                         <div className={"mx-2 d-inline-block"}><strong>ID</strong>: {devTix.id}</div>
                                         <div  className={"mx-2 d-inline-block"}>
                                             <strong>Priority</strong>: {devTix.fields["Microsoft.VSTS.Common.Priority"]}
                                         </div>
-                                        <div className={"mx-2 d-inline-block"}>
+                                        <div className={"mx-2 d-inline-block"} onLoad={() => setBlockStateChange(devTix.fields["System.State"])}>
                                             <strong>State</strong>: {devTix.fields["System.State"]}
                                         </div>
                                         <div  className={"mx-2 d-inline-block"}>
@@ -103,9 +120,11 @@ function UserTickets(props) {
                                             <strong>Assigned To</strong>: {getNameBeforeEmail(devTix.fields["System.AssignedTo"])}
                                         </div>
                                     </Col>
-                                    <Col xs={1} className={"align-self-center d-flex flex-row-reverse"}>
-                                        <a href={`https://dev.azure.com/KrokhalevPavel/MotorQ%20Project/_workitems/edit/${devTix.id}`} rel={"noreferrer"} target={"_blank"} ><TiArrowForwardOutline size={"1.5rem"}/></a>
-                                        <a className={"mx-1"} onClick={() => {setRenderEdit(true); setAllTicketInfo(devTix); showTicketModal(devTix.fields["System.AreaPath"] + "|" + devTix.id);}}><FaPencilAlt size={"1.5rem"}/></a>
+                                    <Col xs={2} className={"align-self-center d-flex flex-row-reverse justify-content-between"}>
+                                        {/*TODO: this is hard coded to our org. fix that.*/}
+                                        <a title={"See DevOps Entry"} className={"mx-1 userTicketBtns"} href={`https://dev.azure.com/KrokhalevPavel/MotorQ%20Project/_workitems/edit/${devTix.id}`} rel={"noreferrer"} target={"_blank"} ><TiArrowForwardOutline size={"1.5rem"}/></a>
+                                        <a title={"Edit Ticket"} className={"mx-1 userTicketBtns"} onClick={() => {setRenderEdit(true); setAllTicketInfo(devTix); showTicketModal(devTix.fields["System.AreaPath"] + "|" + devTix.id);}}><FaPencilAlt size={"1.5rem"}/></a>
+                                        <a title={"Block Ticket"} className={"mx-1 userTicketBtns"} onClick={() => {blockTicket(devTix.id); setBlockStateChange("Blocked");}}><FiAlertOctagon size={"1.5rem"}/></a>
                                     </Col>
                                 </Container>
                             </div>
