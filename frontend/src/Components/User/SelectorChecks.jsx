d23b2ef39aaf3cf47c63b630f383825197d66991
conor <Obrien.Conor@student.greenriver.edu>
2022-06-14 23:42:03 -0700

    altered verbiage a bit. comment and attachment displays looks a bit easier to understand.

--- frontend/src/Components/User/SelectorChecks.jsx
+++ frontend/src/Components/User/SelectorChecks.jsx
@@ -7,14 +7,14 @@ function SelectorChecks({ setRowValue, rowValue }) {
     return (
         <>
             <Row className={"my-2"}>
-                <h6 className={"fw-bold mb-2"}>Comments and Attachments</h6>
+                <h6 className={"fw-bold my-3"}>Comment and Attachment Displays</h6>
                 <Form>
                     <div className={"d-flex justify-content-start"}>
                         <Form.Label htmlFor={"commentSelect"} className={"ms-4 me-2"}>
-                            See Comments <Form.Check className={"ms-2"} type={"radio"} id={"commentCheck"} inline name={"seeChecks"} value={"comments"} defaultChecked onChange={e => setRowValue(e.currentTarget.value)}/>
+                            Comments <Form.Check className={"ms-2"} type={"radio"} id={"commentCheck"} inline name={"seeChecks"} value={"comments"} defaultChecked onChange={e => setRowValue(e.currentTarget.value)}/>
                         </Form.Label>
                         <Form.Label htmlFor={"attachSelect"} className={"mx-2"}>
-                            See Attachments <Form.Check className={"ms-2"} type={"radio"} id={"attachSelect"} inline name={"seeChecks"} value={"attachments"} onChange={e => setRowValue(e.currentTarget.value)}/>
+                            Attachments <Form.Check className={"ms-2"} type={"radio"} id={"attachSelect"} inline name={"seeChecks"} value={"attachments"} onChange={e => setRowValue(e.currentTarget.value)}/>
                         </Form.Label>
                     </div>
                 </Form>
