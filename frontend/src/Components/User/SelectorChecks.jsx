00671f6a144a7e5c33ba933c9008834d49b4847d
conor <Obrien.Conor@student.greenriver.edu>
2022-06-14 23:36:04 -0700

    created checkbox component for swapping between comments and attachments in ticket views

---
+++ frontend/src/Components/User/SelectorChecks.jsx
@@ -0,0 +1,26 @@
+import {Form, Row} from "react-bootstrap";
+import React from "react";
+
+
+function SelectorChecks({ setRowValue, rowValue }) {
+
+    return (
+        <>
+            <Row className={"my-2"}>
+                <h6 className={"fw-bold mb-2"}>Comments and Attachments</h6>
+                <Form>
+                    <div className={"d-flex justify-content-start"}>
+                        <Form.Label htmlFor={"commentSelect"} className={"ms-4 me-2"}>
+                            See Comments <Form.Check className={"ms-2"} type={"radio"} id={"commentCheck"} inline name={"seeChecks"} value={"comments"} defaultChecked onChange={e => setRowValue(e.currentTarget.value)}/>
+                        </Form.Label>
+                        <Form.Label htmlFor={"attachSelect"} className={"mx-2"}>
+                            See Attachments <Form.Check className={"ms-2"} type={"radio"} id={"attachSelect"} inline name={"seeChecks"} value={"attachments"} onChange={e => setRowValue(e.currentTarget.value)}/>
+                        </Form.Label>
+                    </div>
+                </Form>
+            </Row>
+        </>
+    );
+}
+
+export default SelectorChecks;
