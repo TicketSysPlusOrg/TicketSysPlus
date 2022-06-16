ecc28fc2fc23f9d5b5b70e1f2a5ade6e5b2326ce
granny <granny@pl3x.net>
2022-06-16 01:37:14 -0700

    callbacks -> promises

--- backend/models/settingsSchema.js
+++ backend/models/settingsSchema.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 export const settingsSchema = new Schema({
-
     body: {
         type: String,
         description: "body of Settings JSON to be stored in DB"
