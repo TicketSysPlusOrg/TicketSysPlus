a25082ce99f5279e95be2a95363d0b27d4c78c84
conor <Obrien.Conor@student.greenriver.edu>
2022-06-15 21:43:21 -0700

    added 'view none' choice, added doc to top of function, removed unused prop, fixed 'form within form' error

--- frontend/src/Components/User/SelectorChecks.jsx
+++ frontend/src/Components/User/SelectorChecks.jsx
@@ -1,23 +1,29 @@
 import {Form, Row} from "react-bootstrap";
 import React from "react";
 
-
-function SelectorChecks({ setRowValue, rowValue }) {
-
+/**
+ * This component contains radio selectors for displaying comments, attachments, or neither.
+ * @param {props} setRowValue the parent state handler that enables setting the proper state when radio buttons are selected
+ * @returns {JSX.Element} SelectorChecks
+ */
+function SelectorChecks({ setRowValue }) {
     return (
         <>
             <Row className={"my-2"}>
-                <h6 className={"fw-bold my-3"}>Comment and Attachment Displays</h6>
-                <Form>
+                <h5 className={"my-3"}>View Comments or Attachments</h5>
+                <div>
                     <div className={"d-flex justify-content-start"}>
                         <Form.Label htmlFor={"commentSelect"} className={"ms-4 me-2"}>
-                            Comments <Form.Check className={"ms-2"} type={"radio"} id={"commentCheck"} inline name={"seeChecks"} value={"comments"} defaultChecked onChange={e => setRowValue(e.currentTarget.value)}/>
+                            Hide All <Form.Check className={"ms-2"} type={"radio"} id={"none"} inline name={"seeChecks"} value={"none"} defaultChecked onChange={e => setRowValue(e.currentTarget.value)}/>
+                        </Form.Label>
+                        <Form.Label htmlFor={"commentSelect"} className={"ms-4 me-2"}>
+                            Comments <Form.Check className={"ms-2"} type={"radio"} id={"commentCheck"} inline name={"seeChecks"} value={"comments"} onChange={e => setRowValue(e.currentTarget.value)}/>
                         </Form.Label>
                         <Form.Label htmlFor={"attachSelect"} className={"mx-2"}>
                             Attachments <Form.Check className={"ms-2"} type={"radio"} id={"attachSelect"} inline name={"seeChecks"} value={"attachments"} onChange={e => setRowValue(e.currentTarget.value)}/>
                         </Form.Label>
                     </div>
-                </Form>
+                </div>
             </Row>
         </>
     );
